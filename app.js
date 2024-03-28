98df44d6c5661b6a7ac83c891bc472b9269eb42f
syedgulzar786 <syedgulzarmohammad123@gmail.com>
2024-03-28 06:04:31 +0500

    assignment started

---
+++ app.js
@@ -0,0 +1,70 @@
+// different practices of different conditions of for loops
+
+document.write("<h1>");
+
+// task 1-a
+ for (var i = 1; i <= 5;  i++) {
+   for ( var j = 1; j < i + 1; j++){
+         document.write(`${j} `)
+   } 
+   document.write("<br>");
+ }
+
+// task 1-b
+document.write("<h1>");
+ for (var i = 1; i <= 5;  i++) {
+   for ( var j = 1; j <= 5; j++){
+         document.write(`${j} `)
+   } 
+   document.write("<br>");
+ }
+
+ // task 1-c
+document.write("<h1>");
+for (var i = 1; i <= 5;  i++) {
+  for ( var j = 1; j <= 6 - i; j++){
+        document.write(`${j} `)
+  } 
+  document.write("<br>");
+}
+document.write("<br>")
+
+// task 2-b
+   for (var c = 5; c >= 1; c-- ){
+    for(var d = 5; d >= c; d--){
+       document.write( d + " ");
+    }
+       document.write("<br>")
+   }
+document.write("<br>")
+
+// task 2-a
+for (var c = 5; c >= 1; c-- ){
+    for(var d = 5; d >= 1; d--){
+       document.write( d + " ");
+    }
+       document.write("<br>")
+   }
+   document.write("<br>")
+
+//  task  2-c
+for (var a = 1; a <= 5; a++){
+    for(var b = 5; b >= a; b-- ){
+       document.write( b + " ");
+    }
+       document.write("<br>")
+   }
+   document.write("<br>")
+
+// task 3-a
+for (var i = 1; i <= 5;  i++) {
+    for ( var j = 1; j < i + 1; j++){
+        if ( e = "*" ){
+        document.write(e + " ")             
+       }
+    }
+    document.write("<br>");
+
+ } 
+document.write("</h1>");    
+
