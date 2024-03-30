b78575047ead5c906993351f86777511f603c5e3
syedgulzar786 <syedgulzarmohammad123@gmail.com>
2024-03-31 01:16:02 +0500

    submit

---
+++ assign.js
@@ -0,0 +1,94 @@
+// js assignment 5
+
+// const threeDimArray = [
+//       [ ${[${0} ${1} ]}  ${[${2} ${3} `]}  ` ],
+//       [ ${[${1} ${0} ]}  ${[${1} ${2} `]}  ` ],
+//       [ ${[${2} ${1} ]}  ${[${0} ${1} `]}  ` ],
+     
+//     ];
+// document.write(${threeDimArray [0]} <br> ${threeDimArray [1]} <br> ${threeDimArray [2]});
+
+// for(var i = 1; i <= 10; i++){
+//     document.write(`${i} <br> `)
+// }
+
+// var prom = +prompt("knsa table chahye");
+// var prom1 = +prompt("kahan tk chahye");
+// for(var i = 1; i <= prom1; i++ ){
+//     document.write(${prom} x ${i} = ${prom * i} <br>)
+// }
+
+// var fruits = ["apple", "banana", "mango", "orang" , "strawberry"];
+// for (var i = 0; i < fruits.length; i++){
+//     document.write(${fruits[i]} <br>)
+// }
+
+// for(var i = 1; i < 16; i++){
+//     document.write(i + " ")
+// }
+
+// for(var i = 10; i >= 1; i--){
+//     document.write(i + " ")
+// }
+
+// for(var i = 0; i < 21; i = i + 2){
+//     document.write(i + " ")
+// }
+
+// for(var i = 1; i < 26; i = i + 2 ){
+//     document.write(i + " ")
+// }
+// var arr = "k"
+// for(var i = 2; i < 16; i = i + 2){
+//     for(var j = 0;j < arr.length; j++ ){
+//     document.write(i + arr[j])
+//     }
+//     document.write(" ")
+// }
+
+// for(var i = 2; i < 17;i = i + 2){
+//     document.write(i + "k " );
+// }
+// var prom = prompt("what dessert u want in our bakery")
+// var A = ["Cake", "Apple pie", "Cookie", "Chips", "Patties"]
+// var firstWord = prom.slice(0 , 1).toUpperCase();
+// var lastWord = prom.slice(1).toLowerCase();
+// var final = firstWord + lastWord;
+// var match = false
+
+// for(var i = 0;i < A.length;i++){
+//     if(final == A[i]){
+//         match = true
+//         alert(${final} is AVAILABLE in our bakery)
+//         break
+//     }
+// }
+// if(!match){
+//     alert(${final} is NOT AVAILABLE in our bakery)
+// }
+
+// var num = [165, 786, 512, 12, 34, 56, 54, 67, 234, 512, 10, 1200];
+// var largest = num[0]
+
+// for(var i = 0; i < num.length; i++){
+//     if(num[i] > largest){
+//         largest = num[i]   
+//     }
+// }
+
+// document.write("The largest number in the array is: " + largest);
+
+// var num = [165, 786, 512, 12, 34, 56, 54, 67, 234, 512, 10];
+// var largest = num[0]
+
+// for(var i = 0; i < num.length; i++){
+//     if(num[i] < largest){
+//         largest = num[i]   
+//     }
+// }
+
+// document.write("The smallest number in the array is: " + largest);
+
+// for(var i = 1; i <= 20;i++){
+//     document.write(`${5 * i} `)
+// }
